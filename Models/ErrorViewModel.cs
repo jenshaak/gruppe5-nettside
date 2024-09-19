5b3c5589902e0f1e54a3ae3cefcabf0f56b837c4
Kevin Stratvert <jens.haakaas@gmail.com>
2024-09-19 12:56:37 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gruppe5_nettside.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Pages/**/*.{cshtml,html}',
+        './Views/**/*.{cshtml,html}',
+        './wwwroot/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
